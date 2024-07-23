ef71c97127666630cd602c6c820d55207ffed4c2
Дарина Литвин <daryna.lytvyn@nure.ua>
2024-07-23 16:13:29 +0300

    Created datail window

---
+++ ViewModels/DetailViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CryptocurrencieApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencieApp.ViewModels
+{
+    public partial class DetailViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private Cryptocurrencie _currentCryptocurrencie;
+
+        public DetailViewModel(Cryptocurrencie cryptocurrencie) 
+        {
+            _currentCryptocurrencie = cryptocurrencie;
+        
+        }
+    }
+}
